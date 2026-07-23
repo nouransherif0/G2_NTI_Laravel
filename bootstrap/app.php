644c06b50286d81c24c5c7962bda913f57599ac5
figo05-ai <felopater455@gmail.com>
2026-07-23 13:40:23 +0300

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api','throttle:web',
         ]);
 
         $middleware->validateCsrfTokens(except: [
